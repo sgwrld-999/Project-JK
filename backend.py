cb3ad7978c31e4dbb3940711f37a628dabef0d0f
sgwrld-999 <130132265+sgwrld-999@users.noreply.github.com>
2025-12-12 01:30:11 +0530

    Initial commit

---
+++ backend.py
@@ -0,0 +1,85 @@
+# standard imports
+from typing import TypedDict, Annotated
+from dotenv import load_dotenv
+
+# third-party imports
+from langgraph.graph import StateGraph, START, END
+from langchain_core.messages import BaseMessage, HumanMessage
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langgraph.graph.message import add_messages 
+from langgraph.checkpoint.in_memory import InMemorySaver
+
+# load the env variables 
+load_dotenv()
+
+# state 
+class ChatState(TypedDict):
+    """
+        ChatState TypedDict to represent the state of the chat.
+    Args:
+        TypedDict: A dictionary type that maps string keys to values of specified types.
+    
+    Extra:
+    "add_messages": Utility function to add messages to the state which works as "operator.add" but it's optimized for B
+    ase-messages or LangGraph messages.
+    """
+    messages: Annotated[list[BaseMessage], add_messages,"The list of messages exchanged in the chat."]
+    
+
+llm_model = ChatGoogleGenerativeAI(model="gemini-3-pro-preview")
+
+# chat_nodes function
+def chat_nodes(state: ChatState) -> ChatState:
+    """
+        chat_nodes function to generate a response from the LLM model based on the current chat state.
+    Args:
+        state (ChatState): The current state of the chat containing the list of messages.
+    Returns:
+        ChatState: The updated chat state with the new response message added.
+    """
+    
+    messages = state["messages"]
+    
+    response = llm_model.invoke(messages)
+    
+    return {
+        "messages": [response]
+    }
+    
+graph = StateGraph(ChatState)
+
+graph.add_node("chat", chat_nodes)
+
+graph.add_edge(START, "chat")
+graph.add_edge("chat", END)
+
+
+checkpointer = InMemorySaver()
+workflow = graph.compile(checkpointer=checkpointer)
+
+
+# while True:
+#     user_input = input("Type your message: ")
+
+#     if user_input.strip().lower() in ["exit", "bye", "end"]:
+#         print("Ending the chat. Goodbye!")
+#         break
+
+#     response_state = workflow.invoke({"messages": [HumanMessage(content=user_input)]})
+
+#     message = response_state["messages"][-1].content
+
+#     # Case 1: content is a string
+#     if isinstance(message, str):
+#         only_text = message
+
+#     # Case 2: content is a list of chunks (LangGraph formatted)
+#     elif isinstance(message, list) and len(message) > 0:
+#         # find the first text chunk
+#         text_chunks = [c["text"] for c in message if c.get("type") == "text"]
+#         only_text = text_chunks[0] if text_chunks else ""
+
+#     else:
+#         only_text = ""
+
+#     print("AI:", only_text)
---
+++ frontend.py
@@ -0,0 +1,55 @@
+# iteration-3
+import streamlit as st
+
+# third-party imports 
+from langchain_core.messages import HumanMessage
+
+# custom imports 
+from backend import workflow
+
+"""
+Now, to store the files we'll store in the dictionary as follows:
+    {'role' : user, 'content' : 'Hi'}
+    {'role' : assistant , 'content' : 'Hello'}
+"""
+
+CONFIG = {'configuration' : {'thread_id' : 'thread-1'}}
+
+""" 
+Problem:
+Same problem as the previous iteration, the messages are not persistent.
+Once we enter a new message, the previous messages disappear or message history is lost.
+
+To overcome the problem, we will use session state to store the message history.
+"""
+st.title("LangGraph Chatbot")
+
+
+# Initialize message history in session state if it doesn't exist
+if 'message_history' not in st.session_state:
+    st.session_state.message_history = []
+
+# before taking new input, we will display the previous messages
+for message in st.session_state.message_history:
+    with st.chat_message(message['role']):
+        st.markdown(message['content'])
+        
+        
+user_input = st.chat_input("You: ")
+
+if user_input:
+    
+    # now we are storing the message in the dictionary before displaying it
+    st.session_state.message_history.append({'role': 'user', 'content': user_input})
+    with st.chat_message("user"):
+        st.markdown(user_input)
+        
+        
+    response_state = workflow.invoke({"messages": [HumanMessage(content=user_input)]},config=CONFIG)
+    assistant_response = response_state["messages"][-1].content
+    
+    # with a call to your LangGraph agent.
+    # assistant_response = "Hello! How can I assist you today?"
+    st.session_state.message_history.append({'role': 'assistant', 'content': assistant_response})
+    with st.chat_message("assistant"):
+        st.markdown(assistant_response)
